db8aae54576878a3a31f7c681b55e08f3e971406
Yigal Duppen <yigal@publysher.nl>
2015-10-06 19:38:45 +0200

    Extract author if it exists

---
+++ infiksi/__init__.py
@@ -0,0 +1,36 @@
+import bs4
+
+
+def _get_meta_contents_by_name(soup, name):
+    meta_tags = soup.select("meta[name={}]".format(name))
+    if not meta_tags:
+        return None
+
+    return meta_tags[0].attrs['content']
+
+
+def _get_meta_contents_by_property(soup, prop):
+    meta_tags = soup.select("meta[property={}]".format(prop))
+    if not meta_tags:
+        return None
+
+    return meta_tags[0].attrs['content']
+
+
+def parse_contents(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    title = soup.title.text if soup.title else None
+    description = soup.description.text if soup.description else None
+    author = _get_meta_contents_by_name(soup, "author")
+
+    og_title = _get_meta_contents_by_property(soup, "og:title")
+    og_description = _get_meta_contents_by_property(soup, "og:description")
+
+    return dict(
+        title=title,
+        description=description,
+        author=author,
+
+        og_title=og_title,
+        og_description=og_description,
+    )
--- tests/test_extract_data_form_content.py
+++ tests/test_extract_data_form_content.py
@@ -1,48 +1,35 @@
 from unittest import TestCase
+import os.path
+
 import infiksi
 
 
-class ContentExtractionTest(TestCase):
+def _load_fixture(fixture):
+    with open(os.path.join(os.path.dirname(__file__), "fixtures", fixture)) as f:
+        return f.read()
 
+
+class ContentExtractionTest(TestCase):
     def test_extract_empty_html(self):
-        html = """
-        <html></html>
-        """
-        result = infiksi.parse_contents(html)
+        result = infiksi.parse_contents(_load_fixture("empty.html"))
         self.assertIsNotNone(result)
 
     def test_extract_from_title(self):
-        html = """
-        <html><head><title>My Beautiful Page</title></head></html>
-        """
-
-        result = infiksi.parse_contents(html)
+        result = infiksi.parse_contents(_load_fixture("beautifulpage.html"))
         self.assertIsNotNone(result)
         self.assertEquals(result['title'], 'My Beautiful Page')
 
     def test_extract_from_livecodingtv(self):
-        html = """
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <title>Python: retrieving metadata from websites</title>
-  <meta name="description" content=>
-  <meta property="og:site_name" content="Livecoding.tv"/>
-  <meta property="og:title" content="watch people code products live"/>
-  <meta property="og:description" content="Python: retrieving metadata from websites"/>
-  <meta property="og:image" content="/static/img/userdashboard-img.png?h=96c35f43"/>
-  <meta property="og:url" content="/publysher/"/>
-  <meta property="og:type" content="streaming platform"/>
-</head><body>...</body></html>
-        """
-
-        result = infiksi.parse_contents(html)
+        result = infiksi.parse_contents(_load_fixture("livecodingtv_publysher.html"))
         self.assertIsNotNone(result)
 
         self.assertEquals(result['title'], 'Python: retrieving metadata from websites')
         self.assertEquals(result['description'], None)
         self.assertEquals(result['og_title'], 'watch people code products live')
         self.assertEquals(result['og_description'], 'Python: retrieving metadata from websites')
+
+    def test_extract_from_php_fractal(self):
+        result = infiksi.parse_contents(_load_fixture("php_fractal_bad_design.html"))
+        self.assertIsNotNone(result)
+        self.assertEquals(result['title'], 'PHP: a fractal of bad design / fuzzy notepad')
+        self.assertEquals(result['author'], 'Eevee')
