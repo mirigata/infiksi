4b0907093d37830401239f026bb9beee1df76df1
Yigal Duppen <yigal@publysher.nl>
2015-10-06 19:51:28 +0200

    Use OEmbedResponse instead of dictionary

--- infiksi/__init__.py
+++ infiksi/__init__.py
@@ -1,6 +1,19 @@
 import bs4
 
 
+class OEmbedResponse(object):
+
+    def __init__(self, title=None, description=None, author_name=None,
+                 og_title=None, og_description=None):
+        self.author_name = author_name
+        self.description = description
+        self.og_title = og_title
+        self.og_description = og_description
+        self.title = title
+        self.version = '1.0'
+        self.type = 'article'
+
+
 def _get_meta_contents_by_name(soup, name):
     meta_tags = soup.select("meta[name={}]".format(name))
     if not meta_tags:
@@ -26,10 +39,10 @@ def parse_contents(html):
     og_title = _get_meta_contents_by_property(soup, "og:title")
     og_description = _get_meta_contents_by_property(soup, "og:description")
 
-    return dict(
+    return OEmbedResponse(
         title=title,
         description=description,
-        author=author,
+        author_name=author,
 
         og_title=og_title,
         og_description=og_description,
--- tests/test_extract_data_form_content.py
+++ tests/test_extract_data_form_content.py
@@ -14,37 +14,40 @@ class ContentExtractionTest(TestCase):
         result = infiksi.parse_contents(_load_fixture("empty.html"))
         self.assertIsNotNone(result)
 
+        self.assertEquals(result.version, '1.0')
+        self.assertEquals(result.type, 'article')
+
     def test_extract_from_title(self):
         result = infiksi.parse_contents(_load_fixture("beautifulpage.html"))
         self.assertIsNotNone(result)
-        self.assertEquals(result['title'], 'My Beautiful Page')
+        self.assertEquals(result.title, 'My Beautiful Page')
 
     def test_extract_from_livecodingtv(self):
         result = infiksi.parse_contents(_load_fixture("livecodingtv_publysher.html"))
         self.assertIsNotNone(result)
 
-        self.assertEquals(result['title'], 'Python: retrieving metadata from websites')
-        self.assertEquals(result['description'], '')
-        self.assertEquals(result['og_title'], 'watch people code products live')
-        self.assertEquals(result['og_description'], 'Python: retrieving metadata from websites')
+        self.assertEquals(result.title, 'Python: retrieving metadata from websites')
+        self.assertEquals(result.description, '')
+        self.assertEquals(result.og_title, 'watch people code products live')
+        self.assertEquals(result.og_description, 'Python: retrieving metadata from websites')
 
     def test_extract_from_php_fractal(self):
         result = infiksi.parse_contents(_load_fixture("php_fractal_bad_design.html"))
         self.assertIsNotNone(result)
-        self.assertEquals(result['title'], 'PHP: a fractal of bad design / fuzzy notepad')
-        self.assertEquals(result['author'], 'Eevee')
+        self.assertEquals(result.title, 'PHP: a fractal of bad design / fuzzy notepad')
+        self.assertEquals(result.author_name, 'Eevee')
 
     def test_extract_from_medievalbooks(self):
         result = infiksi.parse_contents(_load_fixture("medievalbooksnl_posters.html"))
 
-        self.assertEquals(result['title'], 'Medieval Posters | medievalbooks')
-        self.assertEquals(result['description'],
+        self.assertEquals(result.title, 'Medieval Posters | medievalbooks')
+        self.assertEquals(result.description,
                           'In our modern society there are words everywhere around us, all the time. They are not '
                           'only written in books – that fair and most devoted carrier of text – but also on walls, '
                           'where they appear in all shapes and sizes. Judging from surviving paintings, it appears '
                           'that in medieval times it was less common to have words…')
-        self.assertEquals(result['og_title'], 'Medieval Posters')
-        self.assertEquals(result['og_description'],
+        self.assertEquals(result.og_title, 'Medieval Posters')
+        self.assertEquals(result.og_description,
                           'In our modern society there are words everywhere around us, all the time. They are not '
                           'only written in books – that fair and most devoted carrier of text – but also on walls, '
                           'where they appear in a...')
